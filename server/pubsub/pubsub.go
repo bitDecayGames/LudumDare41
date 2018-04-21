91cdd956d8ba9bf4b917497118a3acd89b2fc08a
Jacob Caban-Tomski <jacque006@gmail.com>
2018-04-21 15:40:16 -0600

    Pub sub meatadata madness

--- server/pubsub/pubsub.go
+++ server/pubsub/pubsub.go
@@ -1,11 +1,12 @@
 package pubsub
 
 import (
-	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/satori/go.uuid"
 )
 
 type Message struct {
@@ -20,42 +21,84 @@ var upgrader = websocket.Upgrader{
 }
 
 type PubSubService interface {
-	AddSubscription(http.ResponseWriter, *http.Request) error
-	SendMessage(Message) error
+	AddSubscription(http.ResponseWriter, *http.Request) (string, error)
+	UpdateSubscription(connectionID string, gameName string, playerName string) error
+	SendMessage(gameName string, msg Message) []error
 }
 
 type pubSubService struct {
-	connections []*websocket.Conn
+	subscriptions []subscription
 }
 
-func (ps *pubSubService) AddSubscription(w http.ResponseWriter, r *http.Request) error {
+type subscription struct {
+	conn         *websocket.Conn
+	connectionID string
+	gameName     string
+	playerName   string
+}
+
+type connectionBody struct {
+	ConnectionID string `json:"connectionID"`
+}
+
+func (ps *pubSubService) AddSubscription(w http.ResponseWriter, r *http.Request) (string, error) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		return err
+		return "", err
 	}
-	ps.connections = append(ps.connections, conn)
-	return nil
+
+	sub := subscription{
+		connectionID: uuid.NewV4().String(),
+		conn:         conn,
+	}
+
+	ps.subscriptions = append(ps.subscriptions, sub)
+
+	body := connectionBody{
+		ConnectionID: sub.connectionID,
+	}
+
+	log.Printf("%+v", body)
+
+	return sub.connectionID, conn.WriteJSON(body)
 }
 
-func (ps *pubSubService) SendMessage(m Message) error {
-	messageBytes, err := json.Marshal(m)
-	if err != nil {
-		return err
-	}
-
-	for _, conn := range ps.connections {
-		err := conn.WriteMessage(websocket.TextMessage, messageBytes)
-		if err != nil {
-			// TODO remove connection?
-			log.Println(err)
+func (ps *pubSubService) UpdateSubscription(connectionID, gameName, playerName string) error {
+	var curSub subscription
+	for _, sub := range ps.subscriptions {
+		if sub.connectionID == connectionID {
+			curSub = sub
+			break
 		}
 	}
 
+	if curSub.connectionID == "" {
+		return fmt.Errorf("subscription not found for connectionID %s", connectionID)
+	}
+
+	curSub.gameName = gameName
+	curSub.playerName = playerName
+
 	return nil
+}
+
+func (ps *pubSubService) SendMessage(gameName string, msg Message) []error {
+	var errors []error
+	for _, sub := range ps.subscriptions {
+		if sub.gameName == gameName {
+			err := sub.conn.WriteJSON(msg)
+			if err != nil {
+				log.Printf("%s, %v, %v", gameName, msg, err)
+				errors = append(errors, err)
+			}
+		}
+	}
+
+	return errors
 }
 
 func NewPubSubService() PubSubService {
 	return &pubSubService{
-		connections: []*websocket.Conn{},
+		subscriptions: []subscription{},
 	}
 }
