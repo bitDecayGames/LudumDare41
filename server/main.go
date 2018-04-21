a0b169819f42c5c86d58412a370c79dfa601958a
Jacob Caban-Tomski <jacque006@gmail.com>
2018-04-21 13:03:43 -0600

    Ping server

--- server/main.go
+++ server/main.go
@@ -1,67 +1,30 @@
 package main
 
 import (
-	"github.com/bitDecayGames/LudumDare41/server/lobby"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 const (
-	jacque = "Jacque"
-	monday = "Monday"
+	port = 8080
 )
 
 func main() {
-	// create 2 player game
-	lobbyService := lobby.NewLobbyService()
-	lobby := lobbyService.NewLobby()
+	host := fmt.Sprintf(":%v", port)
+	log.Printf("Starting server on %s ...", host)
 
-	lobby.AddPlayer(jacque)
-	lobby.AddPlayer(monday)
+	r := mux.NewRouter()
+	r.HandleFunc("/ping", PingHandler)
 
-	// deck := deck.LoadDeck("default")
+	log.Printf("Server started on %s", host)
 
-	// map := map.LoadMap("testLevel")
-	// gameInstance := game.NewGame(lobby, map, deck)
-
-	// gameInstance.Start()
-
-	// gameInstance.DealCards()
-	// gameInstance.SendUpdates() // This sends SanitizedGameStates to each player to set initial state
-
-	// gameInstance.SubmitCards(jacque, cards)
-	// gameInstance.SubmitCards(monday, cards)
-
-	// gameInstance.PrioritizeCards()
-
-	// gameInstance.ResolveCards()
-
-	// if gameInstance.GameOver() {
-	// 	os.Exit(1)
-	// }
-
-	// gameInstance.DealCards()
-	// gameInstance.SendUpdates()
+	http.ListenAndServe(host, r)
 }
 
-// GameState is a fully encompassing snapshot of the state of the game
-type GameState struct {
-}
-
-// SanitizedGameState is a copy of the game state tailored to a single player. It only contains information pertinent to that player
-type SanitizedGameState struct {
-}
-
-// TurnSubmission represents a given player's card choices for the given turn
-type TurnSubmission struct {
-	PlayerID int
-	Tick     int
-	Cards    []Card
-}
-
-// TickUpdate is a data update intended to be sent to the client that contains all needed information on the start state and end state of a given game tick
-type TickUpdate struct {
-	Tick       int
-	PlayerID   int
-	StartState GameState
-	EndState   GameState
-	// Sequences
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	log.Println("Ping")
 }
