02055a5cffdfb665bb959de41eb222f279e4cd65
Jacob Caban-Tomski <jacque006@gmail.com>
2018-04-21 13:52:11 -0600

    Initial websockets

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/bitDecayGames/LudumDare41/server/pubsub"
 	"github.com/gorilla/mux"
 )
 
@@ -12,12 +13,17 @@ const (
 	port = 8080
 )
 
+var pubSubService pubsub.PubSubService
+
 func main() {
 	host := fmt.Sprintf(":%v", port)
 	log.Printf("Starting server on %s ...", host)
 
+	pubSubService = pubsub.NewPubSubService()
+
 	r := mux.NewRouter()
-	r.HandleFunc("/ping", PingHandler)
+	r.HandleFunc("/api/v1/ping", PingHandler).Methods("POST")
+	r.HandleFunc("/api/v1/pubsub", PubSubHandler)
 
 	log.Printf("Server started on %s", host)
 
@@ -25,6 +31,24 @@ func main() {
 }
 
 func PingHandler(w http.ResponseWriter, r *http.Request) {
+	msg := pubsub.Message{
+		Type: "ping",
+	}
+
+	err := pubSubService.SendMessage(msg)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 	log.Println("Ping")
+}
+
+func PubSubHandler(w http.ResponseWriter, r *http.Request) {
+	err := pubSubService.AddSubscription(w, r)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Added pubsub subscription")
 }
---
+++ server/pubsub/pubsub.go
@@ -0,0 +1,56 @@
+package pubsub
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Type string "json:type"
+}
+
+var upgrader = websocket.Upgrader{}
+
+type PubSubService interface {
+	AddSubscription(http.ResponseWriter, *http.Request) error
+	SendMessage(Message) error
+}
+
+type pubSubService struct {
+	connections []*websocket.Conn
+}
+
+func (ps *pubSubService) AddSubscription(w http.ResponseWriter, r *http.Request) error {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return err
+	}
+	ps.connections = append(ps.connections, conn)
+	return nil
+}
+
+func (ps *pubSubService) SendMessage(m Message) error {
+	messageBytes, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+
+	for _, conn := range ps.connections {
+		err := conn.WriteMessage(websocket.TextMessage, messageBytes)
+		if err != nil {
+			// TODO remove connection?
+			log.Println(err)
+		}
+	}
+
+	return nil
+}
+
+func NewPubSubService() PubSubService {
+	return &pubSubService{
+		connections: []*websocket.Conn{},
+	}
+}
