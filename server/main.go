71a00afe809c6494b8e29c5bcbec7fb4e812b20f
Logan <monday.hopscotch@gmail.com>
2018-04-20 22:35:11 -0700

    rough-in pseudo code for server process

---
+++ server/main.go
@@ -0,0 +1,63 @@
+package main
+
+func main() {
+	// create 2 player game
+
+	lobby := game.NewLobby()
+
+	lobby.addPlayer("Jacque")
+	lobby.addPlayer("Monday")
+
+	deck := deck.LoadDeck("default")
+
+	map := map.LoadMap("testLevel")
+	gameInstance := game.NewGame(lobby, map, deck)
+
+	gameInstance.Start()
+
+	gameInstance.DealCards()
+	gameInstance.SendUpdates() // This sends SanitizedGameStates to each player to set initial state
+
+	gameInstance.SubmitCards("Jacque", cards)
+	gameInstance.SubmitCards("Monday", cards)
+
+	gameInstance.PrioritizeCards()
+
+	gameInstance.ResolveCards()
+
+	if gameInstance.GameOver() {
+		os.Exit(1)
+	}
+
+	gameInstance.DealCards()
+	gameInstance.SendUpdates()
+}
+
+// Card represents a single unique playing card
+type Card struct {
+	ID int
+}
+
+// GameState is a fully encompassing snapshot of the state of the game
+type GameState struct {
+}
+
+// SanitizedGameState is a copy of the game state tailored to a single player. It only contains information pertinent to that player
+type SanitizedGameState struct {
+}
+
+// TurnSubmission represents a given player's card choices for the given turn
+type TurnSubmission struct {
+	PlayerID int
+	Tick     int
+	Cards    []Card
+}
+
+// TickUpdate is a data update intended to be sent to the client that contains all needed information on the start state and end state of a given game tick
+type TickUpdate struct {
+	Tick       int
+	PlayerID   int
+	StartState GameState
+	EndState   GameState
+	// Sequences
+}
