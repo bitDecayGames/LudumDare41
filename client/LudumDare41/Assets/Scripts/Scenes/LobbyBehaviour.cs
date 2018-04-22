cbd8175fb05f868d06a8d0b519b2e95c73be0edf
mwingfield <mwingfie@gmail.com>
2018-04-21 20:31:49 -0600

    add initial scenes for lobby management (2)

--- client/LudumDare41/Assets/Scripts/Scenes/LobbyBehaviour.cs
+++ client/LudumDare41/Assets/Scripts/Scenes/LobbyBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using Network;
+﻿using System;
+using Network;
 using UnityEngine;
 using UnityEngine.UI;
 using Utils;
@@ -14,6 +15,10 @@ public class LobbyBehaviour : MonoBehaviour, IUpdateStreamSubscriber {
 	private bool isFirstPlayer = false;
 	
 	void Start () {
+		if (State.lobby == null) throw new Exception("Lobby was null in the Lobby Scene... whoops...");
+
+		title.text = "Lobby: " + State.lobby.code;
+		
 		isFirstPlayer = State.lobby.players.FindIndex(p => p.name == State.myName) == 0;
 		
 		startBtn.gameObject.SetActive(isFirstPlayer);
@@ -27,7 +32,7 @@ public class LobbyBehaviour : MonoBehaviour, IUpdateStreamSubscriber {
 
 	public void RefreshLobbyMembers() {
 		foreach (Transform child in lobbyMembers) {
-			GameObject.Destroy(child.gameObject); // remove all lobby member prefabs
+			Destroy(child.gameObject); // remove all lobby member prefabs
 		}
 		State.lobby.players.ForEach(p => {
 			var lobbyMember = Instantiate(lobbyMemberPrefab);
@@ -37,10 +42,12 @@ public class LobbyBehaviour : MonoBehaviour, IUpdateStreamSubscriber {
 	}
 
 	public void StartGame() {
+		Debug.Log("Trying to start the game");
 		// TODO: send a start game request
 	}
 
 	public void receiveUpdateStreamMessage(string message) {
+		Debug.Log("Received update stream message:" + message);
 		// TODO: if message is something like: "RefreshLobbyMembers" then refresh
 		// TODO: if message is something like: "RequestTick0" then move to game board?
 	}
