e55d93acd387f5d3cc91fb97b6800e7271cf7c98
Jacob Caban-Tomski <jacque006@gmail.com>
2018-04-21 21:45:25 -0600

    Fixes oh my

--- server/game/game_service.go
+++ server/game/game_service.go
@@ -1,18 +1,22 @@
 package game
 
 import (
+	"sync"
+
 	"github.com/bitDecayGames/LudumDare41/server/cards"
 	"github.com/bitDecayGames/LudumDare41/server/gameboard"
 	"github.com/bitDecayGames/LudumDare41/server/lobby"
 	"github.com/bitDecayGames/LudumDare41/server/state"
 )
 
+var mutex = &sync.Mutex{}
+
 type GameService interface {
 	NewGame(*lobby.Lobby, gameboard.GameBoard, cards.CardSet) *Game
 }
 
 type gameService struct {
-	activeGames []Game
+	activeGames []*Game
 }
 
 func NewGameService() GameService {
@@ -29,5 +33,12 @@ func (gs *gameService) NewGame(lobby *lobby.Lobby, board gameboard.GameBoard, ca
 			Deck:    make([]cards.Card, 0),
 		}
 	}
-	return newGame(players, board, cardSet)
+
+	game := newGame(players, board, cardSet)
+
+	mutex.Lock()
+	gs.activeGames = append(gs.activeGames, game)
+	mutex.Unlock()
+
+	return game
 }
