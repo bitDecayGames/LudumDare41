d9b1297567722d415e36071d73f92e935acfb7dc
TannerMoore <shadoninja@gmail.com>
2018-04-22 20:56:01 -0600

    tred change

--- client/LudumDare41/Assets/Scripts/Tank/AnimateTank.cs
+++ client/LudumDare41/Assets/Scripts/Tank/AnimateTank.cs
@@ -10,6 +10,13 @@ public class AnimateTank : MonoBehaviour
     {
         float OffsetX = Time.time * ScrollX;
         float OffsetY = Time.time * ScrollY;
-        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+        var materials = GetComponent<Renderer>().materials;
+        foreach (Material m in materials)
+        {
+            if (m.name == "Material_005")
+            {
+                m.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+            }
+        }
     }
 }
