ed2b0671acc289af9917354494d6768289ef2478
Luke Fisher <lukewfisher@msn.com>
2018-04-23 13:03:13 -0600

    Actionscripts

--- client/LudumDare41/Assets/Scripts/Actions/Death.cs
+++ client/LudumDare41/Assets/Scripts/Actions/Death.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Death : MonoBehaviour {
+public class Death : IActionScript
+{
 
 	// Use this for initialization
 	void Start () {
