7e704d35d112f98d3adcdbf7701efaccc339d9d0
TannerMoore <shadoninja@gmail.com>
2018-04-22 22:32:31 -0600

    scene improvements

--- client/LudumDare41/Assets/Scripts/Tank/AnimateTank.cs
+++ client/LudumDare41/Assets/Scripts/Tank/AnimateTank.cs
@@ -13,10 +13,8 @@ public class AnimateTank : MonoBehaviour
         var materials = GetComponent<Renderer>().materials;
         foreach (Material m in materials)
         {
-            Debug.Log("Material name: " + m.name);
             if (m.name == "TreadAnimation (Instance)")
             {
-                Debug.Log("Found the right material!");
                 m.mainTextureOffset = new Vector2(OffsetX, OffsetY);
             }
         }
